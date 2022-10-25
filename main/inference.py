7e385a71e31c637d48f933d7cdc44341bba193c6
신충호 <skfl78888@gamail.com>
2022-10-25 14:45:22 +0900

    project develope

---
+++ main/inference.py
@@ -0,0 +1,44 @@
+'''
+inference.py: loss 및 AI관련 파이썬 파일
+작성담당자: 이원희박사님, 강경만과장님
+해당 파이썬 구조는 얼마든지 구조변경 가능합니다!
+※ pose_detector class는 신충호 사원이 작성할 예정.
+'''
+
+class inference:
+    def __init__(self):
+        pass
+    
+    def loss(self):
+        pass
+        return None
+    
+    def inference(self):
+        pass
+        return None
+
+
+class Networks:
+    def __init__(self):
+        pass
+    
+    def model(self):
+        pass
+        return None
+    
+    def call(self):
+        pass
+        return None
+
+
+class pose_detector:
+    def __init__(self):
+        pass
+    
+    def model(self):
+        pass
+        return None
+    
+    def call(self):
+        pass
+        return None
---
+++ main/main.py
@@ -0,0 +1,6 @@
+''' 
+main.py: 모듈(class 모음)
+pipelines 설계
+io처리 설계 
+작성담당자: 신충호사원
+'''
