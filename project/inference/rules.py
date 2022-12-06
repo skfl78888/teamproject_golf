1279bdbff964aedcbae4e08e59b2cb114923f44f
신충호 <skfl78888@gmail.com>
2022-12-06 18:16:08 +0900

    1206_rev6

--- project/inference/rules.py
+++ project/inference/rules.py
@@ -51,29 +51,29 @@ class Rules:
     
     def handle(self, action: str, value: dict, coordis: dict):
         if action == 'address':
-            value[action]['어깨선각도'] = self.tan_slope(coordis['left_shoulder'], coordis['right_shoulder'])
+            value[action]['어깨선각도  [어깨 각도(°)]'] = self.tan_slope(coordis['left_shoulder'], coordis['right_shoulder'])
             res = self.center_calc(coordis['left_wrist'], coordis['right_wrist'])
-            value[action]['양손위치'] = self.ratio_interpolation(coordis['left_ankle'],coordis['right_ankle'], res)
-            value[action]['스탠스'] = self.ratio(coordis['left_shoulder'], coordis['right_shoulder'], coordis['left_heel'], coordis['right_heel'])
-            value[action]['상체기울임정도'] = self.ratio(coordis['right_heel'], coordis['right_hip'], coordis['right_hip'], coordis['right_shoulder'])
+            value[action]['양손위치  [양발간격 기준 양손위치 (％)]'] = self.ratio_interpolation(coordis['left_ankle'],coordis['right_ankle'], res)
+            value[action]['스탠스  [어깨대비 양발간격 비율(％)]'] = self.ratio(coordis['left_shoulder'], coordis['right_shoulder'], coordis['left_heel'], coordis['right_heel'])
+            value[action]['상체기울임정도  [상체 길이 대비 하체 길이 비율(％)]'] = self.ratio(coordis['right_heel'], coordis['right_hip'], coordis['right_hip'], coordis['right_shoulder'])
         elif action == 'backswing':
-            value[action]['왼쪽어깨회전'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'], coordis['left_shoulder'])
-            value[action]['왼팔펴짐각도'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])
+            value[action]['왼쪽어깨회전  [무릎간격 기준 왼쪽 어깨위치(％)]'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'], coordis['left_shoulder'])
+            value[action]['왼팔펴짐각도  [왼팔 각도(°)]'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])
         elif action == 'top':
-            value[action]['하체고정'] = self.angle(coordis['right_knee'], coordis['right_hip'], coordis['left_knee'])
-            value[action]['오른쪽다리펴짐각도'] = self.angle(coordis['right_hip'], coordis['right_knee'], coordis['right_ankle'])
+            value[action]['하체고정  [오른무릎-오른골반-왼무릎 각도(°)]'] = self.angle(coordis['right_knee'], coordis['right_hip'], coordis['left_knee'])
+            value[action]['오른쪽다리펴짐각도  [오른쪽 골반-무릎-발 각도(°)]'] = self.angle(coordis['right_hip'], coordis['right_knee'], coordis['right_ankle'])
             res = self.center_calc(coordis['left_shoulder'], coordis['right_shoulder'])    
-            value[action]['스웨이체크'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'], res)
+            value[action]['스웨이체크  [무릎간격 기준 어깨 중점 위치(％)]'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'], res)
         elif action == 'impact':
-            value[action]['행잉백'] = self.angle_2(coordis['left_shoulder'], coordis['left_ankle'])
-            value[action]['오른쪽K라인각도'] = self.angle(coordis['right_shoulder'], coordis['right_hip'], coordis['right_ankle'])
-            value[action]['왼팔펴짐각도'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])
-            value[action]['오른팔펴짐각도'] = self.angle(coordis['right_wrist'], coordis['right_elbow'], coordis['right_shoulder'])
+            value[action]['행잉백  [왼쪽 어깨-무릎선과 Y축 각도(°)]'] = self.angle_2(coordis['left_shoulder'], coordis['left_ankle'])
+            value[action]['오른쪽K라인각도  [오른쪽 어깨-골반-발 각도(°)]'] = self.angle(coordis['right_shoulder'], coordis['right_hip'], coordis['right_ankle'])
+            value[action]['왼팔펴짐각도  [왼팔 각도(°)]'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])
+            value[action]['오른팔펴짐각도  [오른팔 각도(°)]'] = self.angle(coordis['right_wrist'], coordis['right_elbow'], coordis['right_shoulder'])
         elif action == 'follow':
-            value[action]['왼쪽라인'] = self.angle_2(coordis['left_hip'], coordis['left_ankle'])
-            value[action]['치킨윙'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])     
-            value[action]['체중전진이동'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'],cal_point=np.array([0,0,0]) )
-            value[action]['오른쪽다리펴짐각도'] = self.angle(coordis['right_hip'], coordis['right_knee'], coordis['right_ankle'])
+            value[action]['왼쪽라인  [왼쪽 골반-무릎선과 Y축 각도(°)]'] = self.angle_2(coordis['left_hip'], coordis['left_ankle'])
+            value[action]['치킨윙  [왼팔 각도(°)]'] = self.angle(coordis['left_wrist'], coordis['left_elbow'], coordis['left_shoulder'])     
+            value[action]['체중전진이동  [발간격 기준 골반 중점 위치(％)]'] = self.ratio_interpolation(coordis['right_ankle'], coordis['left_ankle'],cal_point=np.array([0,0,0]) )
+            value[action]['오른쪽다리펴짐각도  [오른쪽 골반-무릎-발 각도(°)]'] = self.angle(coordis['right_hip'], coordis['right_knee'], coordis['right_ankle'])
         return value
             
     def tan_slope(self, a: np.ndarray, b: np.ndarray):
